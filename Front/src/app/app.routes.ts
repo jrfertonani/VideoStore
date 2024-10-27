8b89b677e24d569be8d6d49ef81a215ae9af57e8
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-27 17:44:04 -0300

    service video

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { VideosViewComponent } from './components/videos/videos-view/videos-view.component';
+import { VideosCreateComponent } from './components/videos/videos-create/videos-create.component';
+import { VideosUpdateComponent } from './components/videos/videos-update/videos-update.component';
 
 export const routes: Routes = [
 
   {path:'', component: HomeComponent},
-  {path:'videos',component: VideosViewComponent}
+  {path:'videos',component: VideosViewComponent},
+  {path: 'videoCreate', component: VideosCreateComponent},
+  {path: 'videosUpdate', component: VideosUpdateComponent},
+
 
 ];
--- Front/src/app/components/videos/videos-view/videos-view.component.ts
+++ Front/src/app/components/videos/videos-view/videos-view.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Videos } from '../../../models/Videos';
+import { VideosService } from '../../../services/videos/videos.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-videos-view',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './videos-view.component.html',
   styleUrl: './videos-view.component.css'
 })
-export class VideosViewComponent {
+export class VideosViewComponent implements OnInit{
+
+
+  videos: Videos[] = [];
+  videoList: Videos[] = [];
+
+  constructor(private viceosService: VideosService){}
+
+
+  ngOnInit(): void {
+
+
+  }
+
+
 
 }
