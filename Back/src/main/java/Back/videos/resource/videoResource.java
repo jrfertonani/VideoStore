468d2c3b2005245f905c9f428cb6dc8024129b66
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-27 15:44:58 -0300

    video resource

--- Back/src/main/java/Back/videos/resource/videoResource.java
+++ Back/src/main/java/Back/videos/resource/videoResource.java
@@ -1,11 +1,16 @@
 package Back.videos.resource;
 
+import Back.videos.entity.DTO.videoDTO;
+import Back.videos.entity.model.Video;
 import Back.videos.service.videoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -17,4 +22,32 @@ public class videoResource {
 
     @Autowired
     public videoService service;
+
+
+    @PostMapping
+    public ResponseEntity<videoDTO> create(@RequestBody videoDTO DTO){
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(
+                        service.create(DTO)
+                ).toUri();
+        return ResponseEntity.created(uri).body(DTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Video>> findAll(){
+        return ResponseEntity.ok().body(
+                service.findAll()
+                        .stream().map(x -> mapper.map(
+                                x,Video.class)
+                                ).toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<videoDTO> finsById(@PathVariable Long id){
+        return ResponseEntity.ok().body(
+                mapper.map(service.findById(id), videoDTO.class)
+        );
+    }
 }
