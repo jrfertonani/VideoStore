a0dc95927635ff10166a3ee37f74936db1993c3b
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-27 21:11:04 -0300

    video create component ts

--- Front/src/app/components/videos/videos-create/videos-create.component.ts
+++ Front/src/app/components/videos/videos-create/videos-create.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { VideosFormComponent } from "../videos-form/videos-form.component";
+import { Router } from '@angular/router';
+import { VideosService } from '../../../services/videos/videos.service';
+import { Videos } from '../../../models/Videos';
 
 @Component({
   selector: 'app-videos-create',
   standalone: true,
-  imports: [],
+  imports: [VideosFormComponent],
   templateUrl: './videos-create.component.html',
   styleUrl: './videos-create.component.css'
 })
 export class VideosCreateComponent {
+
+  btnAcao = "Create";
+
+  descTitulo = "Create video";
+
+  constructor(private servicoVideo: VideosService, private router: Router){}
+
+  createVideo(video: Videos){
+    this.servicoVideo.create(video).subscribe(videos => {
+      this.router.navigate(['/videos'])
+    })
+  }
+
 
 }
