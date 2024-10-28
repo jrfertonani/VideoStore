ebe954a03302b173fa73b17899303909cee7249b
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-27 22:31:55 -0300

    video update ts

--- Front/src/app/components/videos/videos-update/videos-update.component.ts
+++ Front/src/app/components/videos/videos-update/videos-update.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VideosFormComponent } from "../videos-form/videos-form.component";
+import { ActivatedRoute, Router } from '@angular/router';
+import { VideosService } from '../../../services/videos/videos.service';
+import { Videos } from '../../../models/Videos';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-videos-update',
   standalone: true,
-  imports: [],
+  imports: [VideosFormComponent, CommonModule],
   templateUrl: './videos-update.component.html',
   styleUrl: './videos-update.component.css'
 })
-export class VideosUpdateComponent {
+export class VideosUpdateComponent implements OnInit{
+
+  btnAcao = "Update";
+  descTitulo ="Update video"
+
+  video!: Videos;
+
+
+  constructor(private serviceVideo: VideosService, private router: Router, private route: ActivatedRoute){}
+
+
+  ngOnInit(): void {
+console.log(1)
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+
+    this.serviceVideo.findById(id).subscribe(video => {
+      console.log(2)
+      this.video = video;
+    })
+  }
+
+
+  videosUpdate(video: Videos){
+    console.log(video)
+    this.serviceVideo.update(video).subscribe((video) => {
+
+      this.router.navigate(['/videos'])
+    })
+
+    console.log(video)
+  }
 
 }
