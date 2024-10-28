29c0ebc76119f17b9e43c730544b193692c733dd
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-27 21:10:26 -0300

    video form component ts

--- Front/src/app/components/videos/videos-form/videos-form.component.ts
+++ Front/src/app/components/videos/videos-form/videos-form.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { Videos } from '../../../models/Videos';
+import { VideosService } from '../../../services/videos/videos.service';
 
 @Component({
   selector: 'app-videos-form',
   standalone: true,
-  imports: [],
+  imports:[RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './videos-form.component.html',
   styleUrl: './videos-form.component.css'
 })
-export class VideosFormComponent {
+export class VideosFormComponent implements OnInit{
+
+  @Output() onSubmit = new EventEmitter<Videos>();
+
+
+
+  @Input() btnAcao!: string;
+  @Input() descTitulo!: string;
+
+  constructor(private router: Router){}
+
+
+  videoForm!: FormGroup;
+
+
+
+  ngOnInit(): void {
+    this.videoForm = new FormGroup({
+      idVideos: new FormControl(0),
+      name: new FormControl(''),
+      genres: new FormControl(''),
+      description: new FormControl('')
+    });
+
+  }
+
+
+
+
+  submit(){
+      this.onSubmit.emit(this.videoForm.value);
+  }
+
 
 }
